b4eb2eb8d8f248cf5daddb9acabe44c9cbe2f33a
Thomas Webel <Thomaswebel@gmail.com>
2021-04-02 15:05:48 -0600

    Stub Spring Boot Proj, connected JPA proj as dependency

---
+++ PlaylistDepot/src/main/java/com/skilldistillery/playlist/PlaylistDepotApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.playlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistDepotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistDepotApplication.class, args);
+	}
+
+}
---
+++ PlaylistDepot/src/test/java/com/skilldistillery/playlist/PlaylistDepotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistDepotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
